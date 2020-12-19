aee9fb3f3c99cdbde5390f928c8fdaadc055c7d3
Sean Hart <31716085+seanhart23@users.noreply.github.com>
2020-12-19 12:51:23 -0500

    Update database to Atlas

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var request = require('request');
 
 //CONNECT PACKAGES
 // mongoose.connect("mongodb://localhost/myfinancialplanner_v1", {useMongoClient: true}); 
-mongoose.connect("mongodb://Seanhart23:Maem250123!@ds151528.mlab.com:51528/my_financial_planner", {useMongoClient: true}); 
-// mongoose.connect(process.env.DATABASEURL);    
+// mongoose.connect("mongodb://Seanhart23:Maem250123!@ds151528.mlab.com:51528/my_financial_planner", {useMongoClient: true});
+mongoose.connect("mongodb+srv://seanhart:test@Cluster0.4i40i.mongodb.net/Cluster0?retryWrites=true&w=majority")
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
